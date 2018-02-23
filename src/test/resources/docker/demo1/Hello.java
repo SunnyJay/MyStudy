d6c98c579470b62d8947c4283eb4c62b328c48e3
unknown <nanjun.sun@sinosun.com.cn>
2018-02-23 10:34:20 +0800

    add docker file

---
+++ src/test/resources/docker/demo1/Hello.java
@@ -0,0 +1,16 @@
+package docker.demo1;
+
+/**
+ * Created by Administrator on 2017/10/19.
+ */
+public class Hello
+{
+    public static void main(String[] args) throws InterruptedException
+    {
+        while(true)
+        {
+            Thread.sleep(2000);
+            System.out.println("XXXXXXXXXXXXXXXXX");
+        }
+    }
+}
